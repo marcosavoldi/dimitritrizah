41587a473cb3c41f07fbfbcce55d0062bd2e7f62
Marco <marco.savo.92@gmail.com>
2026-02-14 17:54:10 +0100

    Debug: Add console log for env vars

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,13 @@ import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './context/ThemeContext'
 
+const apiKey = import.meta.env.VITE_FIREBASE_API_KEY;
+console.log("Firebase Config Status:", {
+  apiKeyPresent: !!apiKey,
+  apiKeyLength: apiKey ? apiKey.length : 0,
+  mode: import.meta.env.MODE
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
